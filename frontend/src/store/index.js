a8353610777bf7e0b0d4d684125c48afe1a0c941
varshagade211 <varshagade211@gmail.com>
2022-07-25 11:00:02 -0700

    commented out line 3 'import redux logger'

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,6 +1,6 @@
 
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
-import logger from "redux-logger";
+// import logger from "redux-logger";
 import thunk from 'redux-thunk'
 import {sessionReducer} from './session'
 import spotReducer  from './spots'
